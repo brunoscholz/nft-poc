61a6ee0391b96c656151888132c11cc743bbe2dd
Bruno Ribeiro Scholz <bruno.scholz@possible.com>
2022-06-08 10:40:30 -0300

    recompiled, deployed to goerli

--- truffle-config.js
+++ truffle-config.js
@@ -14,6 +14,20 @@ module.exports = {
       port: 8546,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
+    goerli: {
+      provider: function() {
+        return new HDWalletProvider(
+          privateKeys.split(','),
+          `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`
+        )
+      },
+      // gas: 29999972,
+      // gasPrice: 100000000,
+      network_id: 5,
+      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
     kovan: {
       provider: function () {
         return new HDWalletProvider(
